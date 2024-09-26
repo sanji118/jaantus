748c435b017e5f7e6810ad52ab4d037b38da9bb8
sanji118 <sanjishifa32@gmail.com>
2024-09-27 00:01:00 +0600

    added files

---
+++ domRecap.js
@@ -0,0 +1,25 @@
+/*
+  1. how to use js with html and css
+  2. get element or elements from dom 
+        2-1.getElementById  
+        -->    If the element is not found,  element will contain null
+
+        2-2.getElementByClassName
+        2-3. querySelector
+        2-4. querySelectorAll
+        --> If you want to find all HTML elements that match a specified CSS selector (id, class names, types, attributes, values of attributes, etc), use the querySelectorAll() method.
+3. dynamically add element to the dom : document.createElelment
+        3-1: set value to the innerHTML
+4. get and set value from/to a dom element
+        4-1: use innerText (h1, p, h5, div, span, strong)
+        4-2: use value (input, textArea)
+5. add eventListener (event handler)
+        5-1: click, focus, blur, onchange, keyup
+        5-2: addEvenListener onclick = "myHandler()"
+
+6.use function to reduce duplicate code
+
+7. event bubble and event delegate
+ */
+
+//Array.isArray();
---
+++ ily.js
@@ -0,0 +1,4 @@
+for (let i = 1; i <= 1000; i++) {
+    console.log(`I love you ${i}`);
+  }
+  
